0669c5ed0cc0d49aa9b1ac8fd82c1c87669c391d
Petr Kubeš <petrkubes1997@gmail.com>
2016-11-15 22:27:53 +0100

    Debt dialog design

--- app/src/main/java/cz/petrkubes/payuback/Fragments/DebtsFragment.java
+++ app/src/main/java/cz/petrkubes/payuback/Fragments/DebtsFragment.java
@@ -1,19 +1,23 @@
 package cz.petrkubes.payuback.Fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import cz.petrkubes.payuback.Activities.MainActivity;
 import cz.petrkubes.payuback.Adapters.DebtsAdapter;
 import cz.petrkubes.payuback.Const;
 import cz.petrkubes.payuback.Database.DatabaseHandler;
@@ -63,6 +67,16 @@ public class DebtsFragment extends Fragment implements UpdateableFragment {
         adapter = new DebtsAdapter(getContext(), debts);
         lstDebts.setAdapter(adapter);
 
+        // Set up a click listener for actions with debts
+        lstDebts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Debt debt = (Debt) adapterView.getItemAtPosition(i);
+                showDialog(debt);
+
+            }
+        });
+
         return rootView;
     }
 
@@ -77,5 +91,21 @@ public class DebtsFragment extends Fragment implements UpdateableFragment {
         adapter.clear();
         adapter.addAll(debts);
         adapter.notifyDataSetChanged();
+    }
+
+    private void showDialog(Debt debt) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View debtDialogView = inflater.inflate(R.layout.dialog_debt, null);
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(debtDialogView);
+
+        AlertDialog dialog = builder.create();
+        // display dialog
+        dialog.show();
     }
 }
