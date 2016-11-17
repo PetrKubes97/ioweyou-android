708b211eb59f009c298b9b8afee367225a147859
Petr Kubeš <petrkubes1997@gmail.com>
2016-11-17 16:10:31 +0100

    Paying debts

--- app/src/main/java/cz/petrkubes/payuback/Adapters/DebtsAdapter.java
+++ app/src/main/java/cz/petrkubes/payuback/Adapters/DebtsAdapter.java
@@ -1,7 +1,6 @@
 package cz.petrkubes.payuback.Adapters;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,8 +13,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import cz.petrkubes.payuback.Api.ApiRestClient;
-import cz.petrkubes.payuback.Fragments.UpdateableFragment;
 import cz.petrkubes.payuback.R;
 import cz.petrkubes.payuback.Structs.Debt;
 
@@ -26,10 +23,12 @@ import cz.petrkubes.payuback.Structs.Debt;
 public class DebtsAdapter extends ArrayAdapter<Debt> {
 
     ArrayList<Debt> debts;
+    boolean myDebts = true;
 
-    public DebtsAdapter(Context context, List<Debt> debts) {
+    public DebtsAdapter(Context context, List<Debt> debts, boolean myDebts) {
         super(context, R.layout.item_debt, debts);
         this.debts = (ArrayList<Debt>) debts;
+        this.myDebts = myDebts;
     }
 
     @Override
@@ -47,7 +46,7 @@ public class DebtsAdapter extends ArrayAdapter<Debt> {
             convertView = inflater.inflate(R.layout.item_debt, parent, false);
 
             viewHolder.txtName = (TextView) convertView.findViewById(R.id.txt_name);
-            viewHolder.txtWhat = (TextView) convertView.findViewById(R.id.txt_what);
+            viewHolder.txtWhat = (TextView) convertView.findViewById(R.id.txt_who);
             viewHolder.txtNote = (TextView) convertView.findViewById(R.id.txt_note);
             viewHolder.txtDate = (TextView) convertView.findViewById(R.id.txt_date);
             viewHolder.txtStatus = (TextView) convertView.findViewById(R.id.txt_status);
@@ -66,6 +65,13 @@ public class DebtsAdapter extends ArrayAdapter<Debt> {
         viewHolder.txtNote.setText(debt.note);
         viewHolder.txtDate.setText(df.format(debt.createdAt));
         viewHolder.txtStatus.setText(debt.status);
+
+        // Set color of owned items
+        if (myDebts) {
+            viewHolder.txtWhat.setTextColor(getContext().getResources().getColor(R.color.red));
+        } else {
+            viewHolder.txtWhat.setTextColor(getContext().getResources().getColor(R.color.green));
+        }
 
         // Return the completed view to render on screen
         return convertView;
--- app/src/main/java/cz/petrkubes/payuback/Fragments/DebtsFragment.java
+++ app/src/main/java/cz/petrkubes/payuback/Fragments/DebtsFragment.java
@@ -1,24 +1,34 @@
 package cz.petrkubes.payuback.Fragments;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cz.petrkubes.payuback.Activities.MainActivity;
 import cz.petrkubes.payuback.Adapters.DebtsAdapter;
+import cz.petrkubes.payuback.Api.ApiRestClient;
+import cz.petrkubes.payuback.Api.SimpleCallback;
 import cz.petrkubes.payuback.Const;
 import cz.petrkubes.payuback.Database.DatabaseHandler;
 import cz.petrkubes.payuback.R;
@@ -36,6 +46,22 @@ public class DebtsFragment extends Fragment implements UpdateableFragment {
     private ArrayList<Debt> debts;
     private boolean myDebts = true;
     private View rootView;
+
+    // Dialog elements
+    private Button btnDialogPay;
+    private Button btnDialogEdit;
+    private Button btnDialogCancel;
+    private Button btnDialogPlus;
+    private Button btnDialogMinus;
+    private TextView txtDialogWho;
+    private TextView txtDialogWhat;
+    private TextView txtDialogNote;
+    private TextView txtDialogDate;
+    private SeekBar skbrDialogPayment;
+
+    // Dialog variables
+    private Integer amount;
+    private Handler dialogHandler;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -64,7 +90,7 @@ public class DebtsFragment extends Fragment implements UpdateableFragment {
         }
 
         // Populate the listview
-        adapter = new DebtsAdapter(getContext(), debts);
+        adapter = new DebtsAdapter(getContext(), debts, myDebts);
         lstDebts.setAdapter(adapter);
 
         // Set up a click listener for actions with debts
@@ -76,6 +102,9 @@ public class DebtsFragment extends Fragment implements UpdateableFragment {
 
             }
         });
+
+        // This handler is used for updating amount when user holds plus or minus button
+        dialogHandler = new Handler();
 
         return rootView;
     }
@@ -93,18 +122,167 @@ public class DebtsFragment extends Fragment implements UpdateableFragment {
         adapter.notifyDataSetChanged();
     }
 
-    private void showDialog(Debt debt) {
+    private void showDialog(final Debt debt) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
+        // Inflate layout
         View debtDialogView = inflater.inflate(R.layout.dialog_debt, null);
-
-        // Inflate and set the layout for the dialog
-        // Pass null as the parent view because its going in the dialog layout
         builder.setView(debtDialogView);
+        final AlertDialog dialog = builder.create();
 
-        AlertDialog dialog = builder.create();
+        // Set up buttons, text views and seekbar
+        btnDialogPay = (Button) debtDialogView.findViewById(R.id.btn_pay);
+        btnDialogCancel = (Button) debtDialogView.findViewById(R.id.btn_cancel);
+        btnDialogEdit = (Button) debtDialogView.findViewById(R.id.btn_edit);
+        btnDialogPlus = (Button) debtDialogView.findViewById(R.id.btn_plus);
+        btnDialogMinus = (Button) debtDialogView.findViewById(R.id.btn_minus);
+
+        txtDialogWhat = (TextView)  debtDialogView.findViewById(R.id.txt_what);
+        txtDialogWho = (TextView)  debtDialogView.findViewById(R.id.txt_who);
+        txtDialogNote = (TextView)  debtDialogView.findViewById(R.id.txt_note);
+        txtDialogDate = (TextView) debtDialogView.findViewById(R.id.txt_date);
+
+        skbrDialogPayment = (SeekBar) debtDialogView.findViewById(R.id.skbr_payment);
+
+        txtDialogWhat.setText(debt.what);
+        txtDialogWho.setText(debt.who);
+        txtDialogNote.setText(debt.note);
+        txtDialogDate.setText(debt.createdAtString());
+
+        Log.d(Const.TAG, "AMounte " + String.valueOf(debt.amount));
+
+        if (debt.amount == null) {
+            skbrDialogPayment.setVisibility(View.GONE);
+            btnDialogPlus.setVisibility(View.GONE);
+            btnDialogMinus.setVisibility(View.GONE);
+        } else {
+            skbrDialogPayment.setVisibility(View.VISIBLE);
+            btnDialogPlus.setVisibility(View.VISIBLE);
+            btnDialogMinus.setVisibility(View.VISIBLE);
+
+            skbrDialogPayment.setMax(debt.amount/5);
+            skbrDialogPayment.setProgress(debt.amount);
+
+        }
+
+        if (debt.note.isEmpty()) {
+            txtDialogNote.setVisibility(View.GONE);
+        }
+
+        amount = debt.amount;
+
+        // Update payment while user hold plus or minus button
+        final Runnable runnablePlus = new Runnable() {
+            public void run() {
+                amount += 1;
+
+                btnDialogPay.setText("PAY " + String.valueOf(amount) + " " + debt.currencyString);
+                dialogHandler.postDelayed(this, 150);
+
+                if (amount % 5 == 0) {
+                    skbrDialogPayment.setProgress(amount/5);
+                }
+            }
+        };
+
+        final Runnable runnableMinus = new Runnable() {
+            public void run() {
+                amount -= 1;
+
+                btnDialogPay.setText("PAY " + String.valueOf(amount) + " " + debt.currencyString);
+                dialogHandler.postDelayed(this, 150);
+
+                if (amount % 5 == 0) {
+                    skbrDialogPayment.setProgress(amount/5);
+                }
+            }
+        };
+
+        // Setup seekbar
+        skbrDialogPayment.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                if (i == seekBar.getMax()) {
+                    btnDialogPay.setText("PAY ALL");
+                } else {
+                    amount = i * (debt.amount/seekBar.getMax());
+
+                    btnDialogPay.setText("PAY " + String.valueOf(amount) + " " + debt.currencyString);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        // Set up plus and minus buttons
+        btnDialogPlus.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if (motionEvent.getAction() == motionEvent.ACTION_UP) {
+                    dialogHandler.removeCallbacks(runnablePlus);
+                } else if (motionEvent.getAction() == motionEvent.ACTION_DOWN) {
+                    runnablePlus.run();
+                }
+
+                return false;
+            }
+        });
+
+        btnDialogMinus.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if (motionEvent.getAction() == motionEvent.ACTION_UP) {
+                    dialogHandler.removeCallbacks(runnableMinus);
+                } else if (motionEvent.getAction() == motionEvent.ACTION_DOWN) {
+                    runnableMinus.run();
+                }
+
+                return false;
+            }
+        });
+
+        // Cancel button
+        btnDialogCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        // Pay button
+        btnDialogPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (amount.equals(debt.amount)) { // a) Pay back the whole debt
+                    debt.paidAt = new Date();
+                } else { // b) Decrease payment amount TODO: separate table for payments
+                    debt.amount = debt.amount - amount;
+                }
+
+                debt.version += 1;
+
+                db.addOrUpdateDebt(debt.id, debt);
+
+                ((MainActivity) getActivity()).updateDebts();
+                dialog.cancel();
+            }
+        });
+
         // display dialog
         dialog.show();
     }
