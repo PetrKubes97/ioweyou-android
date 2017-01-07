fbad30edefb986c617c1e423d20c38daf6b22306
Petr Kubeš <petrkubes1997@gmail.com>
2017-01-07 18:06:27 +0100

    typo

--- app/src/main/java/cz/petrkubes/ioweyou/Api/Api.java
+++ app/src/main/java/cz/petrkubes/ioweyou/Api/Api.java
@@ -526,5 +526,4 @@ public class Api {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null;
     }
-
 }
